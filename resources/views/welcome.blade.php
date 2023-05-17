73ce0b7de5ad864c9759975cfb5cef7ad4d29f6b
Asif <asif43997@gmail.com>
2023-05-17 08:05:28 +0500

    Laravel authentication using breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,25 @@
-<h1>this is wlecome/homepage page. </h1>
+
+<script src="https://cdn.tailwindcss.com"></script>
+
+@auth
+
+    <div class="p-6 text-gray-900 dark:text-gray-100 ">
+        Your are logged in as: <strong>{{ auth()->user()->name }}</strong> 
+        <br>
+        Your Phone: <strong>{{ Auth::user()->phone }}</strong>
+    </div>
+
+@else
+    <div class="p-6 text-gray-900 dark:text-gray-100 ">
+        <p>Your are guest user</p>
+    </div>
+
+    <div class="flex">
+        <a class="w-24 h-14 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="{{route('login')}}"> Login
+        </a>
+
+        <a class="w-40 h-14 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="{{route('register')}}">
+            Register 
+        </a>
+    </div>
+@endauth
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutUsController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ConditionsController;
-use App\Http\Controllers\ContactUsController;
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\ServicesController;
-use App\Http\Controllers\SpecialistsController;
-use App\Http\Controllers\WelcomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,51 +14,23 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-
-//HOME PAGE
-Route::get('/',[WelcomeController::class,'home'])->name('home');
-
-Route::get('/contact-us',[ContactUsController::class,'contactUs'])->name('contact_show');
-Route::POST('/save-contact-us',[ContactUsController::class,'saveContactForm'])->name('saveContactForm');
-
-//ABOUT US ROUTE
-Route::get('/aboutUs',[AboutUsController::class,'aboutUs'])->name('aboutUs');
-
-//BLOGS ROUTE
-Route::get('/blogs',[BlogController::class,'blogs'])->name('blogs');
-
-//CONDITIONS WE TREAT ROUTE
-Route::get('/conditions-We-Treat',[ConditionsController::class,'conditions'])->name('conditions');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
-//GALLERY ROUTES 
-Route::get('/gallery',[GalleryController::class,'gallery'])->name('gallery');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/picture-gallery',[GalleryController::class,'pictureGallery'])->name('pictureGallery');
-
-Route::get('/video-gallery',[GalleryController::class,'videoGallery'])->name('videoGallery');
-
-
-
-// OUR SPECIALISTS ROUTES
-Route::get('/our-specialists',[SpecialistsController::class,'specialists'])->name('specialists');
-
-Route::get('/deena-selais',[SpecialistsController::class,'deenaSelais'])->name('deenaSelias');
-
-Route::get('/sarah-farooq',[SpecialistsController::class,'sarahFarooq'])->name('sarahFarooq');
-
-Route::get('/nahid-butti',[SpecialistsController::class,'nahidButti'])->name('nahidButti');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 
+Route::get('/list', [ProfileController::class, 'edit'])->name('profile.edit');
 
-//OUR SERVICES ROUTES
-Route::get('/our-services',[ServicesController::class,'services'])->name('services');
 
-Route::get('/orthopedic',[ServicesController::class,'orthopedic'])->name('orthopedic');
-
-Route::get('/neuro-Rehab',[ServicesController::class,'neuroRehab'])->name('neuroRehab');
-
-Route::get('/personal-trainer',[ServicesController::class,'personalTrainer'])->name('personalTrainer');
-
-Route::get('/lymphodema',[ServicesController::class,'lymphodema'])->name('lymphodema');
-
+require __DIR__.'/auth.php';
